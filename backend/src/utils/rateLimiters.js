919a691369e21cce3c4512f424be363a16cbc05c
RCOM363 <rahullankeppanavar363@gmail.com>
2025-04-10 13:06:33 +0530

    feat(backend): add food post rate limiters

--- backend/src/utils/rateLimiters.js
+++ backend/src/utils/rateLimiters.js
@@ -1,7 +1,6 @@
 import { rateLimit } from "express-rate-limit";
 
 // auth
-
 export const loginLimiter = rateLimit({
   windowMs: 1 * 60 * 1000,
   max: 5,
@@ -14,8 +13,16 @@ export const signUpLimiter = rateLimit({
   message: "Too many signups from this IP. Try again in an hour.",
 });
 
-export const profileLimiter = rateLimit({
+// profile & dashboard
+export const generalGetLimiter = rateLimit({
   windowMs: 10 * 60 * 1000,
   max: 60,
   message: "Too many requests. Try again in 10 minutes",
+});
+
+// food post
+export const postLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 20,
+  message: "Too many food post actions. Please wait a while.",
 });
