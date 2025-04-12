2b3b4d320f16cd01f020a8f8c12bb0db877a4f4b
RCOM363 <rahullankeppanavar363@gmail.com>
2025-04-12 07:43:44 +0000

    feat(frontend): add pooling to fetch foodposts/foodrequests every 10 minutes

--- frontend/src/components/dashboardComponents/DonorDashboard.jsx
+++ frontend/src/components/dashboardComponents/DonorDashboard.jsx
@@ -1,53 +1,58 @@
-import React, { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
-import { fetchDonorFoodPosts } from '../../api/foodPosts';
-import { fetchFoodRequests } from '../../api/foodRequest';
-import FoodPost from './FoodPost';
-import FoodRequest from './FoodRequest';
+import { fetchDonorFoodPosts } from "../../api/foodPosts";
+import { fetchFoodRequests } from "../../api/foodRequest";
+import FoodPost from "./FoodPost";
+import FoodRequest from "./FoodRequest";
 
 const DonorDashboard = () => {
   const [currentFoodPostsPage, setCurrentFoodPostsPage] = useState(1);
   const [currentFoodRequestsPage, setCurrentFoodRequestsPage] = useState(1);
 
-  const { data: posts, isLoading:arePostsLoading } = useQuery({
+  const { data: posts, isLoading: arePostsLoading } = useQuery({
     queryKey: ["donorFoodPosts", currentFoodPostsPage],
-    queryFn:() => fetchDonorFoodPosts(currentFoodPostsPage,2),
-    enabled: true
+    queryFn: () => fetchDonorFoodPosts(currentFoodPostsPage, 2),
+    enabled: true,
   });
 
-  const { data: requests, isLoading:areRequestsLoading } = useQuery({
+  const { data: requests, isLoading: areRequestsLoading } = useQuery({
     queryKey: ["foodRequests", currentFoodRequestsPage],
-    queryFn: () => fetchFoodRequests(currentFoodRequestsPage,2),
-    enabled: true
+    queryFn: () => fetchFoodRequests(currentFoodRequestsPage, 2),
+    enabled: true,
+    refetchInterval: 10 * 60 * 1000, // refetch every 10 minutes
   });
 
   return (
     <div className="bg-gradient-to-r from-green-100 via-teal-100 to-blue-100 min-h-screen py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-7xl mx-auto">
-       <h1 className="text-4xl font-bold text-center text-gray-800 mb-12 "></h1>
+        <h1 className="text-4xl font-bold text-center text-gray-800 mb-12 "></h1>
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
           <div className="bg-white rounded-lg shadow-xl p-6 ">
-            <h2 className="text-2xl font-bold text-gray-700 mb-6">Your Food Posts</h2>
+            <h2 className="text-2xl font-bold text-gray-700 mb-6">
+              Your Food Posts
+            </h2>
             <div className="overflow-hidden rounded-lg border border-gray-200">
-              <FoodPost 
-                posts={posts} 
-                isLoading={arePostsLoading} 
-                currentPage={currentFoodPostsPage} 
-                setCurrentPage={setCurrentFoodPostsPage} 
-                userRole="Donor" 
+              <FoodPost
+                posts={posts}
+                isLoading={arePostsLoading}
+                currentPage={currentFoodPostsPage}
+                setCurrentPage={setCurrentFoodPostsPage}
+                userRole="Donor"
               />
             </div>
           </div>
           <div className="bg-white rounded-lg shadow-xl p-6">
-            <h2 className="text-2xl font-bold text-gray-700 mb-6">Food Requests</h2>
+            <h2 className="text-2xl font-bold text-gray-700 mb-6">
+              Food Requests
+            </h2>
             <div className="overflow-hidden rounded-lg border border-gray-200">
-              <FoodRequest 
-                requests={requests} 
+              <FoodRequest
+                requests={requests}
                 isLoading={areRequestsLoading}
                 currentPage={currentFoodRequestsPage}
                 setCurrentPage={setCurrentFoodRequestsPage}
-                userRole="Donor" 
+                userRole="Donor"
               />
             </div>
           </div>
