a8ff1c45cf9e669037e7884617237bc6bba49549
RCOM363 <rahullankeppanavar363@gmail.com>
2025-04-29 13:54:37 +0000

    refactor(frontend): remove prod stage from dockerfile; remove nginx.conf
    refactor(backend): update cookies options to include frontend domain
    
    refactor: remove frontend-prod from docker-compose; update readme file

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,18 +4,6 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-// const allowedOrigin = process.env.CORS_ORIGIN.split(",");
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (!origin || allowedOrigin.includes(origin)) {
-//         return callback(null, true);
-//       }
-//     },
-//     credentials: true,
-//   })
-// );
-
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN, //allowed to access to the server
