64f39e4a6500cdd5ad1d54c32f0d892b71395039
RCOM363 <rahullankeppanavar363@gmail.com>
2025-04-30 11:49:21 +0000

    refactor: update docker files and ecosys config

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,15 +1,17 @@
+import path from "path";
+
 export default {
   apps: [
     {
       name: "backend-server",
-      script: "./src/index.js",
+      script: path.resolve("src/index.js"),
       instances: "max", // Use maximum CPU cores
       exec_mode: "cluster", // Run in cluster mode for load balancing
       watch: false, // Don't restart on file changes
     },
     {
       name: "email-worker",
-      script: "./src/workers/emailWorker.js",
+      script: path.resolve("src/workers/emailWorker.js"),
       watch: false, // Don't restart on file changes
     },
   ],
